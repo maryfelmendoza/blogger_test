d74b3a56d1875ef174efab34f26600241c1b3323
maryfelmendoza <maryfelmendoza@hotmail.com>
2020-04-25 21:34:06 +0000

    form-based workflow feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,6 @@ BloggerTest::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root to: 'articles#index'
   resources :articles
 end
